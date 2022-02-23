8da1a576792588ac313f99f3038f11120eaae7d9
OliverBark <oliver.bark@icloud.com>
2022-02-23 22:21:15 +0100

    Done BT og Politiken

---
+++ src/main/java/com/example/hjemmeside/HjemmesideApplication.java
@@ -0,0 +1,13 @@
+package com.example.hjemmeside;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HjemmesideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HjemmesideApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hjemmeside/HjemmesideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hjemmeside;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HjemmesideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
